582cb096263f2648b494ab2f2cd122f0ec6dff97
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2023-01-11 08:03:48 +0100

    Pascal's Triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns a list of lists of integers representing the Pascal’s triangle of n"""
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = [[1] * (i + 1) for i in range(n)]
+    for i in range(n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+    return triangle
