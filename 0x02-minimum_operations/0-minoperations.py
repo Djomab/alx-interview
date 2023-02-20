aa38134277c0c2a871e7235499070c9db22b14d3
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2023-02-20 23:42:49 +0100

    update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,18 +6,14 @@ def minOperations(n):
     """calculates the fewest number of operations
     needed to result in exactly n H characters
     """
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        operations = 0
-        i = 2
-        while i * i <= n:
-            while n % i == 0:
-                operations += 1
-                n = n // i
+    operations = 0
+    i = 2
+    while i <= n + 1:
+        if n % i == 0:
+            operations += i
+            n = n / i
+            i = 2
+        else:
             i += 1
-        if n > 1:
-            operations += 1
-        return operations
+
+    return operations
